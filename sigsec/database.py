a3b35052fe2d327d288bf89c35e82061fb1e2fd7
Tanner Stephens <tannerrstephens@gmail.com>
2018-12-02 20:58:45 -0600

    Added user table to database

--- sigsec/database.py
+++ sigsec/database.py
@@ -5,3 +5,12 @@ from flask import current_app
 db = SQLAlchemy(current_app)
 migrate = Migrate(current_app, db)
 
+class User(db.Model):
+  __tablename__ = "users"
+
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+
+  
