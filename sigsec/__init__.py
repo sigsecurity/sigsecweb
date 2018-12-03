60f840eb14e7bf93841f17f6e80ab2d5e4c7e00c
Tanner Stephens <tannerrstephens@gmail.com>
2018-12-02 21:15:43 -0600

    Added requirements.txt

--- sigsec/__init__.py
+++ sigsec/__init__.py
@@ -9,12 +9,7 @@ def create_app(config='sigsec.config.Config'):
     app.config.from_object(config)
 
     with app.app_context():
-        # Database Setup
         from sigsec.database import db
-        url = make_url(app.config['SQLALCHEMY_DATABASE_URI'])
-        if not database_exists(url):
-            db.create_all()
-
         from sigsec.views import views
         app.register_blueprint(views)
 
