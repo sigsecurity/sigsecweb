61584ac0ff69b4fb6b6cbd249ad6c89be8e9e4d1
Tanner Stephens <tannerrstephens@gmail.com>
2018-12-17 23:16:15 -0600

    Modified login and register views to use template

--- sigsec/auth.py
+++ sigsec/auth.py
@@ -18,7 +18,7 @@ auth = Blueprint('auth', __name__)
 @auth.route("/register", methods=['GET', 'POST'])
 def register():
   if request.method == "GET":
-    return render_template("register.html")
+    return render_template("auth/register.html")
   else:
     data = request.get_json()
 
@@ -42,7 +42,7 @@ def register():
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
   if request.method == "GET":
-    return render_template("login.html")
+    return render_template("auth/login.html")
   else:
     data = request.get_json()
 
