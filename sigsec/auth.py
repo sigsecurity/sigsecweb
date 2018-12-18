1c952b4fd000c1f5a96143f7b468ded3d8e3ede6
Tanner Stephens <tannerrstephens@gmail.com>
2018-12-18 00:30:16 -0600

    Updated navbar with some suggestions

--- sigsec/auth.py
+++ sigsec/auth.py
@@ -18,7 +18,7 @@ auth = Blueprint('auth', __name__)
 @auth.route("/register", methods=['GET', 'POST'])
 def register():
   if request.method == "GET":
-    return render_template("auth/register.html")
+    return render_template("auth/register.html", title="Register")
   else:
     data = request.get_json()
 
@@ -42,7 +42,7 @@ def register():
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
   if request.method == "GET":
-    return render_template("auth/login.html")
+    return render_template("auth/login.html", title="Login")
   else:
     data = request.get_json()
 
