976cc5a84bf9a8c8f9ffef21dbda714e81ed5273
Tanner Stephens <tannerstephens@0x99.net>
2018-12-28 16:09:57 -0600

    Homepage (#3)
    * Setup bootstrap framework
    
    * Setup bootstrap framework
    
    * Modified login and register views to use template
    
    * Added navbar template
    
    * Updated navbar with some suggestions
    
    * Added swatch for bootstrap

--- sigsec/auth.py
+++ sigsec/auth.py
@@ -18,7 +18,7 @@ auth = Blueprint('auth', __name__)
 @auth.route("/register", methods=['GET', 'POST'])
 def register():
   if request.method == "GET":
-    return render_template("register.html")
+    return render_template("auth/register.html", title="Register")
   else:
     data = request.get_json()
 
@@ -42,7 +42,7 @@ def register():
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
   if request.method == "GET":
-    return render_template("login.html")
+    return render_template("auth/login.html", title="Login")
   else:
     data = request.get_json()
 
