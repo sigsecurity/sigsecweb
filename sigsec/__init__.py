ebdba172324f0cbc387a7138254a9f425839db94
Tanner Stephens <tannerrstephens@gmail.com>
2019-03-03 23:51:33 -0600

    Remove frontend views

--- sigsec/__init__.py
+++ sigsec/__init__.py
@@ -13,9 +13,6 @@ def create_app(config='sigsec.config.Config'):
     with app.app_context():
         from sigsec.database import db
 
-        from sigsec.views import views
-        app.register_blueprint(views)
-
         from sigsec.auth import auth
         app.register_blueprint(auth)
 
