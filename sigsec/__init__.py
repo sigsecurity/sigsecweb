238f223456ebcacdc82dc0f902832ad2e16f5d51
Tanner Stephens <tannerrstephens@gmail.com>
2019-03-05 04:38:42 -0600

    Fixes nginx proxy

--- sigsec/__init__.py
+++ sigsec/__init__.py
@@ -1,13 +1,44 @@
 from flask import Flask
-from werkzeug.contrib.fixers import ProxyFix
 from sqlalchemy_utils import database_exists
 from sqlalchemy.engine.url import make_url
 import os
 
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the 
+    front-end server to add these headers, to let you quietly bind 
+    this to a URL other than / and to an HTTP scheme that is 
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
 
 def create_app(config='sigsec.config.Config'):
     app = Flask(__name__)
-    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.wsgi_app = ReverseProxied(app.wsgi_app)
     app.config.from_object(config)
 
     with app.app_context():
